ec00447f9da199403539e3fbd1f9b0549d584859
Daniel T <Danieltwarg@gmail.com>
2024-12-19 14:33:27 -0500

    login may be okay now, possible fix

--- src/Develop/client/src/api/authAPI.tsx
+++ src/Develop/client/src/api/authAPI.tsx
@@ -1,8 +1,32 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
-const login = async (userInfo: UserLogin) => {
+
   // TODO: make a POST request to the login route
-}
+  const login = async (userInfo: UserLogin) => {
+    try {
+    
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userInfo),
+      });
+
+      if (!response.ok) {
+        const errorMessage = await response.json();
+        throw new Error('Login failed: ' + errorMessage.message);
+      };
+
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      console.log('Login failed: ', error);
+
+      return Promise.reject(error);
+    }
+  }
 
 
 
