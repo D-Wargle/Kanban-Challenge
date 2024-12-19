225c5e1d21c66cdf255865ce9ebb1d00f99aa94e
Daniel T <Danieltwarg@gmail.com>
2024-12-19 16:14:10 -0500

    did utils getProfile, loggedIn, isTokenExpired functions

--- src/Develop/client/src/utils/auth.ts
+++ src/Develop/client/src/utils/auth.ts
@@ -3,18 +3,32 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (!token) return null;
+    return jwtDecode<JwtPayload>(token);
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decodedToken = jwtDecode<JwtPayload>(token);
+      if (decodedToken?.exp && decodedToken?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (error) {
+      return false;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    
   }
 
   login(idToken: string) {
